3ac54a3f13c357efb971171190b8c05f5a0636f1
Hassan Khwileh <hassan.khwileh@gmail.com>
2025-07-19 09:27:57 +0400

    Initial agentic  commit

--- udemy/langchain/react-langchain/main.py
+++ udemy/langchain/react-langchain/main.py
@@ -1,35 +1,74 @@
+from typing import Union, List
+
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.schema import AgentAction, AgentFinish
+from langchain.tools import Tool, tool
+from langchain.tools.render import render_text_description
+from langchain.schema.runnable import RunnableLambda
+from langchain.agents.output_parsers import ReActSingleInputOutputParser  # Import the missing parser
+
+
 load_dotenv()
-from langchain.agents import tool
+
 
 @tool
-def get_text_length(text:str)->int:
-    """Return the length of the text by characters"""
+def get_text_length(text: str) -> int:
+    """Returns the length of a text by characters"""
+    print(f"get_text_length enter with {text=}")
+    text = text.strip("'\n").strip(
+        '"'
+    )  # stripping away non alphabetic characters just in case
+
     return len(text)
 
-if __name__ =="__main__":
-    print("hello react agent")
-    print(get_text_length.invoke(input={"text":"Hassan KKhwileh"}))
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool wtih name {tool_name} not found")
+
+
+if __name__ == "__main__":
+    print("Hello ReAct LangChain!")
     tools = [get_text_length]
 
     template = """
-            Answer the following questions as best you can. You have access to the following tools:
+    Answer the following questions as best you can. You have access to the following tools:
 
-            {tools}
+    {tools}
+    
+    Use the following format:
+    
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+    
+    Begin!
+    
+    Question: {input}
+    Thought:
+    """
 
-            Use the following format:
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join([t.name for t in tools]),
+    )
 
-            Question: the input question you must answer
-            Thought: you should always think about what to do
-            Action: the action to take, should be one of [{tool_names}]
-            Action Input: the input to the action
-            Observation: the result of the action
-            ... (this Thought/Action/Action Input/Observation can repeat N times)
-            Thought: I now know the final answer
-            Final Answer: the final answer to the original input question
+    llm = ChatOpenAI(temperature=0, stop = ["\nObservation", "Observation"])
+    agent = {"input":lambda x :x["input"]} | prompt | llm | ReActSingleInputOutputParser()
+    agent_steps : Union[AgentAction , AgentFinish] = agent.invoke({"input":"what is the text length if 'DOG' in characters ?"}
+                       )
+    if isinstance (agent_steps,AgentAction):
+        tool_name = agent_steps.tool
+        tool_to_use = find_tool_by_name(tools,tool_name)
+        tool_input = agent_steps.tool_input
+        observation = tool_to_use.func(str(tool_input))
+        print(f"{observation=}")
 
-            Begin!
-
-            Question: {input}
-            Thought:{agent_scratchpad}
-"""
---
+++ udemy/langchain/react-langchain/test.py
@@ -0,0 +1,15 @@
+
+
+from langchain.tools import tool
+
+@tool(description="this function is used to find text length ")
+def get_text_length(txt:str)->int:
+    return len(txt)
+ValueError(f"Some issue happen when i try to get the text lenght")
+
+
+tools = [get_text_length]
+tool = tools[0]
+
+res = tool.func("dog")
+print(res)

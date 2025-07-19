65d3f6166889b01255c0fddb837c5a674df5c297
Hassan Khwileh <hassan.khwileh@gmail.com>
2025-07-19 10:40:12 +0400

    Initial commit

--- udemy/langchain/ice_breaker.py
+++ udemy/langchain/ice_breaker.py
@@ -14,19 +14,18 @@ from output_parsers import summary_parser, Summary
 def ice_break_with(name: str) -> Tuple[Summary, str]:
     linkedin_username = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url=linkedin_username, mock=True
+        linkedin_profile_url=linkedin_username, mock=False
     )
 
-    twitter_username = twitter_lookup_agent(name=name)
-    tweets = scrape_user_tweets(username=twitter_username, mock=True)
+    #twitter_username = twitter_lookup_agent(name=name)
+    #tweets = scrape_user_tweets(username=twitter_username, mock=True)
 
     summary_template = """
     given the information about a person from linkedin {information},
-    and their latest twitter posts {twitter_posts} I want you to create:
     1. A short summary
     2. two interesting facts about them 
 
-    Use both information from twitter and Linkedin
+    Use information for Linkedin
     \n{format_instructions}
     """
     summary_prompt_template = PromptTemplate(
@@ -41,7 +40,7 @@ def ice_break_with(name: str) -> Tuple[Summary, str]:
 
     chain = summary_prompt_template | llm | summary_parser
 
-    res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
+    res = chain.invoke(input={"information": linkedin_data})
 
     return res, linkedin_data.get("profile_pic_url")
 

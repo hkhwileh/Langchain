abebcbf42178f67e33b4147059eef2a51ea98f2e
Hassan Khwileh <hassan.khwileh@gmail.com>
2025-07-20 12:42:19 +0400

    chat with pdfs

--- udemy/langchain/VectorDB/chat_with_pdf.py
+++ udemy/langchain/VectorDB/chat_with_pdf.py
@@ -1,6 +1,10 @@
 from dotenv import load_dotenv
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings,OpenAI
+from langchain_community.vectorstores import FAISS
+
+load_dotenv()
 
 if __name__ =="__main__":
     pdf_path = "/Users/dc-hassan/Desktop/AI/My Repo/SampleCode/udemy/langchain/VectorDB/verdicts.pdf"
@@ -9,3 +13,7 @@ if __name__ =="__main__":
     text_splitter = CharacterTextSplitter(chunk_size=1000,chunk_overlap=30,separator="\n")
     doc = text_splitter.split_documents(documents)
     print(doc[2])
+
+    #start embedding
+    embedding = OpenAIEmbeddings()
+    vectorstore= FAISS.from_documents(doc,embedding)
